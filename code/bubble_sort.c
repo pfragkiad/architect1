d7499772105af6e51ceb4149546b7116d1876b7f
Pavlos <pfragkiad@auth.gr>
2019-11-20 07:41:59 +0200

    Added bubble sort code.

---
+++ code/bubble_sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h> //printf
+#include <stdlib.h> //rand()
+
+//Utility function for array printing
+void printArray(int* arr, int size) 
+{ 
+  for (int i=0; i < size; i++) 
+    printf("%d ", arr[i]); 
+  printf("\n"); 
+} 
+
+//Utility function for swapping 2 int values
+void swap(int *xp, int *yp) 
+{ 
+  int temp = *xp; 
+  *xp = *yp; 
+  *yp = temp; 
+} 
+
+  
+//Bubble sort function
+void bubbleSort(int* arr, int n) 
+{ 
+  for (int i = 0; i < n - 1; i++)       
+    for (int j = 0; j < n - i - 1; j++)  
+      if (arr[j] > arr[j + 1]) 
+        swap(&arr[j], &arr[j + 1]); 
+} 
+  
+
+//Driver program
+int main(int argc, char** argv) 
+{ 
+   //we allow the user to give a custom size for the array
+   size_t n = argc == 2 ? atoi(argv[1]) : 100;
+  
+   //fill with random values between 0 and 99
+   int* arr = (int*)malloc(n * sizeof(int));
+   for(size_t i = 0; i < n; i++)
+     arr[i] = rand() % 100;
+
+   //print unsorted array
+   printf("Unsorted array: \n"); 
+   printArray(arr, n); 
+
+   //sort the array and print the results
+   bubbleSort(arr, n); 
+   printf("Sorted array: \n"); 
+   printArray(arr, n); 
+
+   //release the array
+   free(arr);
+   return EXIT_SUCCESS; 
+} 
